63bcfff0d06fac8921448bbe149300816d05b9a3
Christyn Garcia <christynsandusky@gmail.com>
2021-07-06 20:47:42 -0500

    adding commenting to files

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 console.log("working");
 
 
-var submitBtn = $('#submitBtn');
-var dayTemp = $('#dayTemp');
-var dayWind = $('#dayWind');
-var dayUV = $('#dayUV');
-var dayHumidity = $('#dayHumidity');
-var dayCity = $('#dailyCity');
-var dayIcon = $('#dayIcon');
+var submitBtn = $('#submitBtn'); // submit button
+var dayTemp = $('#dayTemp'); // daily temperature
+var dayWind = $('#dayWind'); //daily wind
+var dayUV = $('#dayUV'); //daily uv index
+var dayHumidity = $('#dayHumidity'); //daily humidity
+var dayCity = $('#dailyCity'); // city
+var dayIcon = $('#dayIcon'); //icon
 
 
 
-var recentSearch = $('#recentSearch');
-var currentDayWeather = $('#currentDayWeather');
-var fiveDayWeather = $('#fiveDayWeather');
+var recentSearch = $('#recentSearch'); // displays recent searches
+var currentDayWeather = $('#currentDayWeather'); //current weather
+var fiveDayWeather = $('#fiveDayWeather'); //five day weather
 
 // Javascript Variables
 var userInput
 var saveCity = [];
 
+// listens for button click and user input
 submitBtn.click(function() {
     userInput = $('#userInfo').val();
     updateDiv();
@@ -27,41 +28,50 @@ submitBtn.click(function() {
     display();
 })
 
+// pulls in user input
 function getWeather() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&units=imperial&appid=d5560a2195af1c81ce054ec9f5539ee6')
         .then(function(response) {
             return response.json();
         })
-        .then(function(data) {
-            console.log(data)
-            dayCity.text(data.name + " " + moment().format('MM/DD/YYYY'));
 
-            fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&exclude=hourly,alerts&units=imperial&appid=d5560a2195af1c81ce054ec9f5539ee6')
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(function(data2) {
-                    console.log(data2);
-                    dayUV.text("UV Index: " + data2.current.uvi);
-                    dayIcon.prepend('<img src = "http://openweathermap.org/img/wn/' + data2.current.weather[0].icon + '@2x.png" />');
-                    dayTemp.text("Temp: " + data2.current.temp + "°F");
-                    dayWind.text("Wind: " + data2.current.wind_speed + " MPH");
-                    dayHumidity.text("Humidity: " + data2.current.humidity + "%");
+    //displays current date
+    .then(function(data) {
+        console.log(data)
+        dayCity.text(data.name + " " + moment().format('MM/DD/YYYY'));
+
+        // gets users latitude and longitude
+        fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&exclude=hourly,alerts&units=imperial&appid=d5560a2195af1c81ce054ec9f5539ee6')
+            .then(function(response) {
+                return response.json();
+            })
+
+        // gathers current weather, uvi, temp, wind speed and humidity
 
 
-                    $('.myCardTemp').each(function(i) { $(this).text("Temp: " + data2.daily[i].temp.day); });
+        .then(function(data2) {
+            console.log(data2);
+            dayUV.text("UV Index: " + data2.current.uvi);
+            dayIcon.prepend('<img src = "http://openweathermap.org/img/wn/' + data2.current.weather[0].icon + '@2x.png" />');
+            dayTemp.text("Temp: " + data2.current.temp + "°F");
+            dayWind.text("Wind: " + data2.current.wind_speed + " MPH");
+            dayHumidity.text("Humidity: " + data2.current.humidity + "%");
 
-                    $('.myCardPhoto').each(function(i) { $(this).prepend('<img src = "http://openweathermap.org/img/wn/' + data2.daily[i].weather[0].icon + '@2x.png" />'); });
 
-                    $('.myCardWind').each(function(i) { $(this).text("Wind: " + data2.daily[i].wind_speed + "MPH"); });
+            $('.myCardTemp').each(function(i) { $(this).text("Temp: " + data2.daily[i].temp.day); });
 
-                    $('.myCardHumidity').each(function(i) { $(this).text("Humidity: " + data2.daily[i].humidity + '%'); });
+            $('.myCardPhoto').each(function(i) { $(this).prepend('<img src = "http://openweathermap.org/img/wn/' + data2.daily[i].weather[0].icon + '@2x.png" />'); });
 
-                    $('.myCardDate').each(function(i) { $(this).text(moment().add(1 + i, 'days').format("MM/DD/YYYY")); });
-                })
+            $('.myCardWind').each(function(i) { $(this).text("Wind: " + data2.daily[i].wind_speed + "MPH"); });
+
+            $('.myCardHumidity').each(function(i) { $(this).text("Humidity: " + data2.daily[i].humidity + '%'); });
+
+            $('.myCardDate').each(function(i) { $(this).text(moment().add(1 + i, 'days').format("MM/DD/YYYY")); });
         })
+    })
 }
 
+// saves searched cities to local storage
 function check() {
     if (localStorage.getItem("cities") != null) { return JSON.parse(localStorage.getItem("cities")); } else { return saveCity; }
 }
@@ -70,6 +80,8 @@ function save() {
     saveCity = check();
     saveCity.unshift(userInput);
 
+
+    // limits recent searches to display only the last 5 
     if (saveCity.length > 5) {
         saveCity.pop();
         localStorage.setItem("cities", JSON.stringify(saveCity));
